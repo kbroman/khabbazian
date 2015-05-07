40c59a2ab2d03fcd78e80ee8c28e6a01c6ef8775
moha <khabbazian@wisc.edu>
2015-05-06 20:33:55 -0500

    changed the dir of bm.r

--- R/bm.R
+++ R/bm.R
@@ -1,22 +1,14 @@
 
-delta  = 1/32;
-t.seq  = seq(1,100, delta);  
-
-sigma2 = 0.001;
-x.t = brownian.motion(t.seq, sigma2 = sigma2);
-y.t = brownian.motion(t.seq, sigma2 = sigma2);
-my.bm.plot( x.t, y.t);
-
 
 ## sample a 2d brownian motion
-brownian.motion <- function( time.seq, mu = 0, sigma2 = 1){
+get.bm <- function( time.seq, mu = 0, sigma2 = 1){
     x.t = rnorm(n = length(t.seq), mean = mu, sd = sqrt(sigma2));
     x.t = cumsum(x.t);
     return(x.t);
 }
 
 ## plotting
-my.bm.plot  <- function(x, y){
+plot.bm  <- function(x, y){
     y.bound = max( abs(y)) + 1;
     x.bound = max( abs(x)) + 1;
     plot(x, y, type = "l", xlim = c(-x.bound, x.bound),
