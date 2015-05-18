898c87dd81a7d8cb88dd04cb616a4e98d5edf507
moha <khabbazian@wisc.edu>
2015-05-18 15:09:07 -0500

    adding package

--- MoBrownie/R/bm.R
+++ MoBrownie/R/bm.R
@@ -1,48 +1,52 @@
-# get.bm 
-#'generate a sample 2d Brownian motion (BM)
-#'
-#'@param time.seq input time sequence.
-#'@param mu expected value of BM or the starting value.
-#'
-#'@return x.t a series of numbers 
-#'@export
-#'
-#'@examples
-#'get.bm(c(1,2,3,4))
-#' 
-get.bm <- function( time.seq, mu = 0, sigma2 = 1){
-    ## the variance has to be postive
-    stopifnot( sigma2 > 0);
-    ## at leat one time point is required
-    stopifnot( length(time.seq) > 0 );
-
-    x.t = rnorm(n = length(t.seq), mean = mu, sd = sqrt(sigma2));
-    x.t = cumsum(x.t);
-    return(x.t);
-}
-
 # plot.bm 
 #'plot a 2D Brownian motion (BM)
 #'
 #'@param x input x axis 
 #'@param y input y axis 
+#'@param ... extra arguments to be passed to plot
 #'
-#'@export
+#'@return none
 #'
 #'@examples
-#'delta  = 1/32;
-#'t.seq  = seq(1,100, delta);  
-#'
-#'sigma2 = 0.001;
-#'x.t = get.bm(t.seq, sigma2 = sigma2);
-#'y.t = get.bm(t.seq, sigma2 = sigma2);
-#'plot.bm( x.t, y.t);
+#'delta  = 1/32
+#'t.seq  = seq(1, 100, delta)
+#'sigma2 = 0.001
+#'x.t    = get.bm(t.seq, sigma2 = sigma2)
+#'y.t    = get.bm(t.seq, sigma2 = sigma2)
+#'plot.bm(x.t, y.t)
 #' 
-plot.bm  <- function(x, y){
+#'@export
+plot.bm  <- function(x, y, ...){
     stopifnot(length(x) == length(y));
 
-    y.bound = max( abs(y)) + 1;
-    x.bound = max( abs(x)) + 1;
+    y.bound = max(abs(y)) + 1;
+    x.bound = max(abs(x)) + 1;
     plot(x, y, type = "l", xlim = c(-x.bound, x.bound),
-         ylim = c(-y.bound, y.bound));
+         ylim = c(-y.bound, y.bound), ... );
 }
+
+
+# get.bm 
+#'generate a sample 2d Brownian motion (BM)
+#'
+#'@param time.seq input time sequence.
+#'@param mu input expected value of BM or the starting value.
+#'@param sigma2 input variance 
+#'
+#'@return x.t a series of numbers 
+#'
+#'@examples
+#'get.bm(c(1,2,3,4))
+#' 
+#'@export
+get.bm <- function(time.seq, mu = 0, sigma2 = 1){
+    ## the variance has to be postive
+    stopifnot( sigma2 > 0);
+    ## at leat one time point is required
+    stopifnot( length(time.seq) > 0 );
+
+    x.t = rnorm(n = length(time.seq), mean = mu, sd = sqrt(sigma2));
+    x.t = cumsum(x.t);
+    return(x.t);
+}
+
